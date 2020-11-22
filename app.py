83a2bb142c9d4f2c096821f4c9cd4bf31c7db2d3
Pmca96 <pedromca96@gmail.com>
2020-11-22 19:31:59 +0000

    Alteração do template

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',  page='index')
 
 @app.route('/1')
 def index1():
-    return render_template('index1.html')
+    return render_template('index1.html',  page='index1' )
